f87f138b83e134ce376921fc7a0fb50143f8580d
ijrussell <ijrussell@fastmail.co.uk>
2023-12-02 08:37:56 +0000

    Simplified parse function

--- Day1/code.fsx
+++ Day1/code.fsx
@@ -1,48 +1,31 @@
 open System
 open System.IO
-open System.Collections.Generic
 
 module Part1 =
     let mapping = 
         [1..9]
         |> List.map (fun item -> string item, item)
-        |> dict
+        |> Map.ofList
 
 module Part2 =
     let mapping = 
+        ([1..9] |> List.map (fun item -> string item, item)) @
         [ "one", 1; "two", 2; "three", 3; "four", 4; "five", 5; "six", 6; "seven", 7; "eight", 8; "nine", 9 ]
-        |> dict
+        |> Map.ofList
 
-let inline buildSearchValues (mapping:IDictionary<string,int>) = 
-    let values = mapping.Values |> Seq.map string
-    let keys : string seq = mapping.Keys
-    Seq.concat [ keys; values ]
-    |> Seq.distinct
+let parse (mapping:Map<string,int>) (input:string) =
+    let getValue (key:string) =
+        let (success, value) = Int32.TryParse(key)
+        if success then value else mapping[key]
+    let matches = [
+        for i in 0..input.Length-1 do
+            for item in mapping.Keys do
+                if input.Substring(i).StartsWith(item) then (item, i) ]
+    let first = matches |> List.minBy snd |> fst
+    let last = matches |> List.maxBy snd |> fst
+    (10 * (first |> getValue)) + (last |> getValue)
 
-let inline parseFirst (seachValues:string seq) (input:string) =
-    seachValues 
-    |> Seq.map (fun item -> item, input.IndexOf item)
-    |> Seq.filter (fun item -> snd item >= 0)
-    |> Seq.minBy snd 
-    |> fst
-
-let inline parseLast (seachValues:string seq) (input:string) =
-    seachValues
-    |> Seq.map (fun item -> item, input.LastIndexOf item)
-    |> Seq.maxBy snd
-    |> fst 
-
-let parse (mapping:IDictionary<string,int>) =
-    let searchValues = buildSearchValues mapping
-    fun (input:string) ->
-        let getValue (input:string) =
-            let (success, value) = Int32.TryParse(input)
-            if success then value 
-            else mapping[input]
-        let calc f = input |> f searchValues |> getValue
-        10 * calc parseFirst + calc parseLast
-
-let run (mapping:IDictionary<string,int>) (fileName:string) =
+let run (mapping:Map<string,int>) (fileName:string) =
     Path.Combine(__SOURCE_DIRECTORY__, fileName)
     |> File.ReadAllLines
     |> Array.map (parse mapping)
