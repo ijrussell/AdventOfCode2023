b2a662a99eb97940d9b19901c21f08a5f8f538ef
ijrussell <ijrussell@fastmail.co.uk>
2023-12-04 14:30:30 +0000

    Day4 initial version

--- Day3/code.fsx
+++ Day3/code.fsx
@@ -5,15 +5,25 @@ let isNumeric (input:string) =
     let (success, _) = Int32.TryParse(input)
     success
 
-let hasAdjacentSymbol (symbols:(int * int * string) array) (item:(int * int * string)) =
+let hasAdjacentSymbol (symbols:(int * int * string) list) (item:(int * int * string)) =
     let (i, j, _) = item
     [
         for di in -1..1 do
             for dj in -1..1 do
                 symbols
-                |> Array.exists (fun (x, y, _) -> x = i + di && y = j + dj)
+                |> List.exists (fun (x, y, _) -> x = i + di && y = j + dj)
     ]
     |> List.filter (fun value -> value)
+    |> fun items -> items <> []
+
+let hasAdjacentGear (gear:int * int) (numbers:(int * int) list) =
+    let (x, y) = gear
+    [
+        for i, j in numbers do
+            for dx in -1..1 do
+                for dy in -1..1 do
+                    if x + dx = i && y + dy = j then (i, j)
+    ]
     |> fun items -> items <> []
 
 let joinNumbers (numbers:(int * int * string * bool) list) =
@@ -34,30 +44,67 @@ let combine (parts:(int * int * string * bool) list list) =
     |> List.rev
     |> List.map (fun items ->
         items
-        |> List.fold (fun (str, inc) (_, _, nstr, ninc) -> str + nstr, inc || ninc) ("", false)
-        |> fun (str, inc) -> int str, inc
+        |> List.fold (fun (str, inc, pos) (i, j, nstr, ninc) -> str + nstr, inc || ninc, (i, j)::pos) ("", false, [])
+        |> fun (str, inc, pos) -> int str, inc, pos
     )
 
-let run (fileName:string) =
+let readData (fileName:string) =
     Path.Combine(__SOURCE_DIRECTORY__, fileName)
     |> File.ReadAllLines
-    |> Array.mapi (fun i data -> 
-        data 
-        |> Seq.mapi (fun j item -> (i, j, string item))
-        |> Seq.toArray
-    )
-    |> Array.concat
-    |> Array.filter (fun (_, _, item) -> item <> ".")
-    |> Array.partition (fun (_, _, item) -> item |> isNumeric)
+    |> fun rows ->
+        [
+            for i in 0..rows.Length-1 do
+                for j in 0..rows[i].Length-1 do
+                    let item = rows[i][j]
+                    if item <> '.' then (i, j, string item)
+        ]
+    |> List.partition (fun (_, _, item) -> item |> isNumeric)
+
+let runPart1 (fileName:string) =
+    fileName
+    |> readData
     |> fun (numbers, symbols) ->
         numbers
-        |> Array.map (fun (i, j, number) -> (i, j, number, hasAdjacentSymbol symbols (i, j, number)))
-    |> Array.toList
+        |> List.map (fun (i, j, number) -> (i, j, number, hasAdjacentSymbol symbols (i, j, number)))
     |> joinNumbers
     |> combine
-    |> List.choose (fun (value, inc) -> if inc then Some value else None)
+    |> List.choose (fun (value, inc, _) -> if inc then Some value else None)
     |> List.sum
 
-let part1test = run "test-data-part1.txt"
+let runPart2 (fileName:string) =
+    let (numbers, symbols) =
+        fileName
+        |> readData
+    let gears = symbols |> List.filter (fun (_, _, item) -> item = "*")
+    let adjacent =
+        numbers
+        |> List.map (fun (i, j, number) -> (i, j, number, hasAdjacentSymbol gears (i, j, number)))
+        |> joinNumbers
+        |> combine
+        |> List.filter (fun (_, isAdjacent, _) -> isAdjacent)
+    gears 
+    |> List.map (fun (i, j, _) -> 
+        adjacent 
+        |> List.filter (fun (_, _, items) ->
+            hasAdjacentGear (i, j) items
+        )
+    )
+    |> List.filter (fun items -> 
+        items 
+        |> List.filter (fun (_, _, nums) -> nums |> List.length > 1)
+        |> List.length > 1
+    )
+    |> List.map (fun items -> 
+        items 
+        |> List.map (fun (num, _, _) -> num)
+        //|> List.reduce (*)
+    )
+    //|> List.sum
 
-let part1 = run "actual-data.txt"
+let part1test = runPart1 "test-data-part1.txt"
+
+let part1 = runPart1 "actual-data.txt"
+
+let part2test = runPart2 "test-data-part1.txt"
+
+let part2 = runPart2 "actual-data.txt"
---
+++ Day4/code.fsx
@@ -0,0 +1,73 @@
+open System
+open System.IO
+
+let (|LessThan|Match|GreaterThan|) (first:int list, second:int list) =
+    match first, second with
+    | hw::_, hc::_ when hc = hw -> Match
+    | hw::_, hc::_c when hc < hw -> LessThan
+    | _ -> GreaterThan
+
+let findMatches (winners:int list) (chosen:int list) =
+    let rec loop acc remW remC =
+        match remW, remC with
+        | [], _ | _, [] -> acc
+        | Match -> loop (remW.Head::acc) remW.Tail remC.Tail
+        | LessThan -> loop acc remW remC.Tail
+        | GreaterThan -> loop acc remW.Tail remC
+    loop [] winners chosen
+
+let parseNumbers (input:string) =
+    input.Split(" ")
+    |> Array.choose (fun item -> 
+        if String.IsNullOrWhiteSpace item then None 
+        else item.Trim() |> int |> Some)
+    |> Array.sort
+    |> Array.toList
+
+let parse (input:string) =
+    match input.Split(":") with
+    | [|_;numbers|] -> 
+        match numbers.Split("|") with
+        | [|winners;chosen|] -> 
+            findMatches (winners.Trim() |> parseNumbers) (chosen.Trim() |> parseNumbers)
+            |> List.length
+        | _ -> failwith $"Invalid input: {input}"
+    | _ -> failwith $"Invalid input: {input}"
+
+let zzz (games:int array) =
+    let rec loop acc next =
+        match next with 
+        | [] -> acc
+        | _ -> 
+            next
+            |> List.collect (fun game->
+                let bonus = games[game]
+                match bonus with
+                | 0 -> []
+                | _ -> [game+1..game+bonus]
+            )
+            |> loop (next @ acc) 
+    loop [] (games |> Array.toList |> List.mapi (fun i _ -> i))
+
+let runPart1 (fileName:string) =
+    Path.Combine(__SOURCE_DIRECTORY__, fileName)
+    |> File.ReadAllLines
+    |> Array.map parse
+    |> Array.sumBy (fun len -> pown 2 (len-1))
+
+let runPart2 (fileName:string) =
+    Path.Combine(__SOURCE_DIRECTORY__, fileName)
+    |> File.ReadAllLines
+    |> Array.map parse
+    |> zzz
+    |> List.length 
+    // |> List.groupBy id
+    // |> List.map (fun (id, items) -> id+1, items |> List.length)   
+
+let part1test = runPart1 "test-data.txt"
+
+let part1 = runPart1 "actual-data.txt"
+
+let part2test = runPart2 "test-data.txt"
+
+let part2 = runPart2 "actual-data.txt"
